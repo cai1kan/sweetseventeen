d0780daee43e6a1323e46c8ada76bec2559feb5a
cai1kan <caileen.wang@gmail.com>
2026-02-06 09:13:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function submitYes() {
+  document.getElementById("attendance").value = "YES";
+  document.querySelector("form").submit();
+  localStorage.setItem("guestName", document.querySelector(".name-input").value);
+  window.location.href = "menu.html";
+}
+
+function submitNo() {
+  document.getElementById("attendance").value = "NO";
+  document.querySelector("form").submit();
+  window.location.href = "thankyou-no.html";
+}
+
+function submitMenu() {
+  document.getElementById("menuName").value =
+    localStorage.getItem("guestName") || "";
+  document.querySelector("form").submit();
+  window.location.href = "thankyou-yes.html";
+}
